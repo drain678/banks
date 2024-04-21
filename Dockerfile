be0d493ff28016cfeae8f9480afd186534352a79
drain678 <ekaterina1.zorina@gmail.com>
2024-04-21 18:15:22 +0300

    set up docker and requirements

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.11 
+
+WORKDIR /app 
+
+COPY banks/ ./banks
+COPY banks_app/ ./banks_app
+
+COPY requirements.txt . 
+
+COPY manage.py . 
+
+RUN pip install -r requirements.txt 
+RUN  
+
+# CMD ["echo", "hello"]
+
+# runs the production server
+CMD ["python", "manage.py", "migrate"] 
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- banks/settings.py
+++ banks/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'banks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "banks", # TODO: configure env 
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "my-db",
+        "PORT": "5432",
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

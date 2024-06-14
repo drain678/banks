6080c6a9ff2fc5e3f0b5d6f69077bf332f364797
drain678 <ekaterina1.zorina@gmail.com>
2024-06-14 11:25:57 +0300

    fix by flake8 apps.py

--- banks_app/apps.py
+++ banks_app/apps.py
@@ -1,6 +1,10 @@
+"""Django application configuration for the banks_app."""
+
 from django.apps import AppConfig
 
 
 class BanksAppConfig(AppConfig):
+    """Configuration for the banks_app Django application."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'banks_app'

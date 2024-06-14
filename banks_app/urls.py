27767d23f2dd39f99995e1c0d4da00bce36b68f1
drain678 <ekaterina1.zorina@gmail.com>
2024-06-14 12:05:34 +0300

    fix by flake8 urls.py

--- banks_app/urls.py
+++ banks_app/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LogoutView
+"""URL configuration for banks_app."""
+
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -21,22 +21,29 @@ urlpatterns = [
     path('clients/<uuid:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
     path('profile/', views.profile_view, name='profile'),
     path('logout/', views.logout_view, name='logout'),
-    path('delete_bank_account/<uuid:pk>/', views.delete_bank_account_view, name='delete_bank_account'),
+    path(
+        'delete_bank_account/<uuid:pk>/',
+        views.delete_bank_account_view, name='delete_bank_account',
+    ),
     path('create_bank_account/', views.create_bank_account_view, name='create_bank_account'),
     path('bank_accounts/', views.bank_accounts_view, name='bank_accounts'),
-    path('transactions/<uuid:pk>/', views.TransactionDetailView.as_view(), name='transaction_detail'),
+    path(
+        'transactions/<uuid:pk>/',
+        views.TransactionDetailView.as_view(),
+        name='transaction_detail',
+    ),
     path('user_transactions/', views.UserTransactionListView.as_view(), name='user_transactions'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('register/', views.register_view, name='register'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('create_client/', views.create_client, name='create_client'),
     path('create_bank/', views.create_bank_view, name='create_bank'),
     path('delete_bank/<uuid:pk>/', views.delete_bank_view, name='delete_bank'),
     path('create_transaction/', views.create_transaction, name='create_transaction'),
     path('confirm_transaction/', views.confirm_transaction, name='confirm_transaction'),
     path('transactions/', views.transactions_view, name='transactions'),
-    path('delete_transaction/<uuid:pk>/', views.delete_transaction_view, name='delete_transaction'),
+    path(
+        'delete_transaction/<uuid:pk>/',
+        views.delete_transaction_view,
+        name='delete_transaction',
+    ),
 ]

0f0e1da04b2ee2de4f30311f3ba173f5b18eab3f
drain678 <ekaterina1.zorina@gmail.com>
2024-06-15 19:01:02 +0300

    fix by flake8

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -2,7 +2,6 @@
 
 from decimal import Decimal
 
-from tests import config
 from banks_app.models import Bank, BankAccount, Client, Transaction
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
@@ -10,6 +9,8 @@ from django.test import Client as TestClient
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
+
+from tests import config
 
 
 def extract_data_from_response(response, keys_to_find):

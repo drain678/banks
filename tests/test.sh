66910d7c5ba3b7fd2c3555fd6cec7c11d1c244ae
drain678 <ekaterina1.zorina@gmail.com>
2024-06-15 19:33:30 +0300

    fix test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -5,5 +5,5 @@ export PG_PORT=5432
 export PG_USER=test
 export PG_PASSWORD=test
 export PG_DBNAME=postgres
-export SECRET_KEY=4o7wrqsup*pc*m_etd$mu$8klfl2r$l1_073a+-j_tkvq9a+b7
+export SECRET_KEY=django-insecure-m=@g$0^4bxl&h^p8a8n^yt%rcv8qlz5(5kly(&06a0ztqcf-t7
 python3 manage.py test $1

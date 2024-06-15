ca493a0783b010cd7637ef57d4662f05bc371883
drain678 <ekaterina1.zorina@gmail.com>
2024-06-15 19:04:45 +0300

    fix by flake8

--- banks_app/views.py
+++ banks_app/views.py
@@ -10,10 +10,9 @@ from rest_framework import status, viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
+from .models import Bank, BankAccount, BankClient, Client, Transaction
 from banks_app import forms
 from banks_app import serializers
-
-from .models import Bank, BankAccount, BankClient, Client, Transaction
 
 
 def is_admin(user):

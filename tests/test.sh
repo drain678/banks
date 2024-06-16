4c24bb971e944374c0b2326fb471da51573eeae6
drain678 <ekaterina1.zorina@gmail.com>
2024-06-16 18:20:38 +0300

    update2 test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PG_HOST=127.0.0.1
-export PG_PORT=5432
-export PG_USER=test
-export PG_PASSWORD=test
-export PG_DBNAME=postgres
+export POSTGRES_HOST=127.0.0.1
+export POSTGRES_PORT=5432
+export POSTGRES_USER=test
+export POSTGRES_PASSWORD=test
+export POSTGRES_DB=postgres
 export SECRET_KEY=1grz2o3i$fh+r9mxr_5hn*2#&qf5bf=c6fk^j$0pw3&5q5zzz5
 python3 manage.py test $1
